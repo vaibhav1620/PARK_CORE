916d613e26663d23e1c6cc3952b289bea7fe176f
vaibhav1620 <43521611+vaibhav1620@users.noreply.github.com>
2020-12-21 21:13:44 +0530

    update 21/12

--- app.js
+++ app.js
@@ -2,11 +2,18 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const path = require('path');
+const parkModel = require('./models/parks');
 
 mongoose.connect('mongodb://localhost:27017/park-core',{
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", ()=> {
+    console.log("Database connection open");
 });
 
 app.set('view engine','ejs');
@@ -19,6 +26,13 @@ app.get('/', function(req,res){
 app.get('/home', function(req,res){
     res.render('home.ejs')
 })
+
+app.get('/makepark', async function(req,res){
+    const park = new parkModel({title: 'PEC University',location: 'sector 12 Chandigarh'});
+    await park.save();
+    res.send(park);
+})
+
 
 app.listen(9000,()=>{
     console.log("SERVING ON PORT 9000");
